c94b05f982e3467c03bbc63ce7debe2c3f694924
Fabricio Oliveira <fabricioque@gmail.com>
2017-04-29 00:44:17 -0300

    fix teste

--- app/jobs/fota_job.rb
+++ app/jobs/fota_job.rb
@@ -1,13 +1,24 @@
 
 Dashing.scheduler.every '10s' do
-  p 'teste'
-  Dashing.send_event('welcome',   { title: 'Last Version Fota', text: 'teste' })
+  Dashing.send_event('last-version',   { title: 'Last Version Fota', text: 'teste' })
 end
 
 def get_last_version
   response = HTTParty.get('http://localhost:8080/api/v1/release/last')
   return "Nenhuma release cadastrada :(" if response.code == :no_content
   values = JSON.parse response.body
-  p "#{values['model']} - #{values['version']}"
   "#{values['model']} - #{values['version']}"
 end
+
+
+
+# current_valuation = 0
+#
+# Dashing.scheduler.every '10s' do
+#   last_valuation = current_valuation
+#   current_valuation = rand(10)
+#
+#   Dashing.send_event('valuation', { current: current_valuation, last: last_valuation })
+#   Dashing.send_event('synergy',   { value: rand(10) })
+#   Dashing.send_event('welcome',   { title: 't1', text: 't2'})
+# end
